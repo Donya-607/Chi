2b6572fa27a304c5892e8b341c5f714fd664b40b
tkm1211 <tkm.1239@gmail.com>
2019-10-29 15:01:27 +0900

    effect関係

--- Chi/Chi/SceneEffect.cpp
+++ Chi/Chi/SceneEffect.cpp
@@ -1,23 +1,43 @@
 #include "scene.h"
+#include "sceneManager.h"
 
 
 void SceneEffect::init()  
 {
 	isStack = false;
+	keyCnt = 0;
 
 	flashParticle.Set();
 	flashParticle.ImGuiDataInit();
 
+	bubbleParticle.Set();
+	bubbleParticle.ImGuiDataInit();
+
 	bossAI.Init();
+
 }
 void SceneEffect::update()
 {
+	if (GetKeyState('T') < 0)
+	{
+		if (keyCnt == 0)
+		{
+			pSceneManager->setNextScene(new sceneTitle, false);
+		}
+		keyCnt++;
+	}
+	else
+	{
+		keyCnt = 0;
+	}
+
 	flashParticle.Emit();
+	bubbleParticle.Emit();
 	bossAI.Update();
 }
 void SceneEffect::render()
 {
-
+	clearWindow(0.5f, 0.5f, 0.5f, 1.0f);
 }
 void SceneEffect::uninit()
 {
@@ -27,7 +47,10 @@ void SceneEffect::imGui()
 {
 	ImGui::Begin("Particle");
 	flashParticle.ImGui();
+	bubbleParticle.ImGui();
 	ImGui::End();
+
+	flashParticle.UseImGui();
 
 	ImGui::Begin("BossAI");
 	bossAI.ImGui();
