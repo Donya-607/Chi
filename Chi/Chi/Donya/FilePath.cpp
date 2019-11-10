4079ce1fccbe2a6684b09b77dca91b3531c19496
Donya <alias>
2019-11-10 14:11:36 +0900

    モデルを更新，ファイルパスを変更

--- Chi/Chi/Donya/FilePath.cpp
+++ Chi/Chi/Donya/FilePath.cpp
@@ -37,35 +37,35 @@ std::string GetModelPath( ModelAttribute modelAttribute )
 	switch ( modelAttribute )
 	{
 	case ModelAttribute::PlayerIdle:
-		return "./Data/model/Player/MDL_Player_Wait.fbx";				// break;
+		return "./Data/model/Player/MDL_Player_Wait.fbx";					// break;
 	case ModelAttribute::PlayerRun:
-		return "./Data/model/Player/MDL_Player_Run.fbx";				// break;
+		return "./Data/model/Player/MDL_Player_Run.fbx";					// break;
 	case ModelAttribute::PlayerAtk:
-		return "./Data/model/Player/MDL_Player_Attack.fbx";				// break;
+		return "./Data/model/Player/MDL_Player_Attack.fbx";					// break;
 	case ModelAttribute::PlayerDefend:
-		return "./Data/model/Player/MDL_Player_Shield.fbx";				// break;
+		return "./Data/model/Player/MDL_Player_Shield.fbx";					// break;
 	case ModelAttribute::Stage:
-		return "./Data/model/Stage/MDL_Stage01.fbx";					// break;
+		return "./Data/model/Stage/MDL_Stage01.fbx";						// break;
 	case ModelAttribute::GolemIdle:
-		return "./Data/model/Boss/MDL_Boss03_Wait.fbx";					// break;
+		return "./Data/model/Boss/Boss03/MDL_Boss03_Wait.fbx";				// break;
 	case ModelAttribute::GolemAtkSwing:
-		return "./Data/model/Boss/MDL_Boss03_Attack_Bandgap.fbx";		// break;
+		return "./Data/model/Boss/Boss03/MDL_Boss03_Attack_Bandgap.fbx";	// break;
 	case ModelAttribute::GolemAtkFast:
-		return "./Data/model/Boss/MDL_Boss03_Attack_Continuous.fbx";	// break;
+		return "./Data/model/Boss/Boss03/MDL_Boss03_Attack_Continuous.fbx";	// break;
 	case ModelAttribute::GolemAtkRotate:
-		return "./Data/model/Boss/MDL_Boss03_Attack_Rotate.fbx";		// break;
+		return "./Data/model/Boss/Boss03/MDL_Boss03_Attack_Rotate.fbx";		// break;
 	case ModelAttribute::KnightIdle:
-		return "./Data/model/Boss/MDL_Boss02_Wait.fbx";					// break;
+		return "./Data/model/Boss/Boss02/MDL_Boss02_Wait.fbx";				// break;
 	case ModelAttribute::KnightRunFront:
-		return "./Data/model/Boss/MDL_Boss02_Walk_Forward.fbx";			// break;
+		return "./Data/model/Boss/Boss02/MDL_Boss02_Walk_Forward.fbx";		// break;
 	case ModelAttribute::KnightAtkExplosion:
-		return "./Data/model/Boss/MDL_Boss02_Attack_Explosion.fbx";		// break;
+		return "./Data/model/Boss/Boss02/MDL_Boss02_Attack_Explosion.fbx";	// break;
 	case ModelAttribute::KnightAtkSwing:
-		return "./Data/model/Boss/MDL_Boss02_Attack_Swing.fbx";			// break;
+		return "./Data/model/Boss/Boss02/MDL_Boss02_Attack_Swing.fbx";		// break;
 	case ModelAttribute::KnightAtkRaid:
-		return "./Data/model/Boss/MDL_Boss02_Attack_JumpAttack.fbx";	// break;
+		return "./Data/model/Boss/Boss02/MDL_Boss02_Attack_JumpAttack.fbx";	// break;
 	case ModelAttribute::KnightFxExplosion:
-		return "./Data/model/Effect/EFE_Boss02_Explosion.fbx";			// break;
+		return "./Data/model/Effect/EFE_Boss02_Explosion.fbx";				// break;
 	default:
 		assert( !"Error : Specified unexpect model type." ); break;
 		break;
